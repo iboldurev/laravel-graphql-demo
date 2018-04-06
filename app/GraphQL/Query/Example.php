88de434285c429005ca277266761d89614250892
Ivan Boldyrev <iboldurev@gmail.com>
2018-04-06 16:10:16 +0300

    Added support graphQl

---
+++ app/GraphQL/Query/Example.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Query;
+use GraphQL\Type\Definition\ResolveInfo;
+
+class Example extends Query
+{
+    protected $attributes = [
+        'name' => 'Example',
+        'description' => 'Example query'
+    ];
+
+    public function type()
+    {
+        return Type::nonNull(Type::string());
+    }
+
+    public function args()
+    {
+        return [
+            
+        ];
+    }
+
+    public function resolve($root, $args, $context, ResolveInfo $info)
+    {
+        return 'Example query';
+    }
+}
